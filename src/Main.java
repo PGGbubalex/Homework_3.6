4fff06a30ec833a7da3e97bcdd9ca6d16059c52b
Igor <99yearago99@gmail.com>
2022-09-22 21:09:57 +0300

    Task 6 complete

--- src/Main.java
+++ src/Main.java
@@ -20,6 +20,6 @@ public class Main {
         int totalWeightOfTheDish = totalWeightOfBananas + totalWeightOfEggs + totalWeightOfMilk + totalWeightOfBriquettesOfIceCream;
         System.out.println("Вес получившегося блюда в граммах составляет " + totalWeightOfTheDish + " в граммах, или " + (totalWeightOfTheDish / gramsPerKilogram) + " в килограммах");
 
-
+        //task 6 complete
     }
 }
